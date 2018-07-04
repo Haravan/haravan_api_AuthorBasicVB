2b47f1eab942cff682ace14cc4906c70b3241327
Cường Lưu Mạnh <cuong.luumanh@haravan.com>
2018-07-04 14:31:44 +0700

    init source

---
+++ SampleAuthorBasic/Module1.vb
@@ -0,0 +1,48 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.Text
+Imports System.Threading.Tasks
+
+Module Module1
+    Sub Main()
+        Dim shop As String = "https://lublue7.myharavan.com/admin"
+
+        Dim apiKey As String = "a75c7d8bd76e08ee03344a21cc0b0b45"
+
+        Dim password As String = "5ce968fcb49bc90e9d89f6dd7279c447"
+
+        Dim token = HashToToken(apiKey, password)
+
+        Dim result = SendRequest(shop + "/products.json", HttpMethod.Get, token).Result
+
+    End Sub
+
+    Public Function HashToToken(ByVal apiKey As String, ByVal password As String) As String
+        Dim authbytes As Byte() = Encoding.ASCII.GetBytes($"{apiKey}:{password}")
+        Return Convert.ToBase64String(authbytes)
+    End Function
+
+    Private Async Function SendRequest(ByVal url As String, ByVal method As HttpMethod, ByVal access_token As String, ByVal Optional data As String = Nothing) As Task(Of String)
+        Dim rq = New HttpRequestMessage(method, url)
+        rq.Headers.Authorization = New AuthenticationHeaderValue("Basic", access_token)
+        rq.Headers.Accept.Add(New MediaTypeWithQualityHeaderValue("application/json"))
+        If data IsNot Nothing Then rq.Content = New StringContent(data, Encoding.UTF8, "application/json")
+
+        Dim result As String = ""
+
+        Using client As HttpClient = New HttpClient()
+            Using response As HttpResponseMessage = Await client.SendAsync(rq)
+                Using content As HttpContent = response.Content
+                    ' Get contents of page as a String.
+                    result = Await content.ReadAsStringAsync()
+
+                End Using
+            End Using
+        End Using
+
+        Return result
+    End Function
+End Module
---
+++ SampleAuthorBasic/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SampleAuthorBasic")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SampleAuthorBasic")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("78ebdd9e-5b9a-44c1-bb0c-98b2bfa34924")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
